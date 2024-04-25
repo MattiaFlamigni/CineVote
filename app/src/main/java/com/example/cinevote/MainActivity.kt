32cbb376f8cea93602492e7759a2560bafad56ff
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-04-25 23:03:42 +0200

    #

--- app/src/main/java/com/example/cinevote/MainActivity.kt
+++ app/src/main/java/com/example/cinevote/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.example.cinevote.screens.HomeScreen
+
 import com.example.cinevote.screens.auth.SignUpasswordScreen
 
 
@@ -31,6 +33,7 @@ class MainActivity : ComponentActivity() {
                     //Greeting("Android")
                     //LoginForm()
                     //SignUp()
+                    //HomeScreen()
 
                 }
             }
