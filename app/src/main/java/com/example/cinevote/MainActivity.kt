e9ec586e4e8f7270e618cfd07644f81d98d69517
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-04-26 17:58:57 +0200

    performed back arrow system behaviour: from HomeScreen now app is closed if navigateUp

--- app/src/main/java/com/example/cinevote/MainActivity.kt
+++ app/src/main/java/com/example/cinevote/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.cinevote
 
 import android.os.Bundle
+import android.window.OnBackInvokedDispatcher
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.cinevote.screens.HomeScreen
@@ -19,8 +24,14 @@ import com.example.cinevote.screens.auth.SignUpasswordScreen
 import com.example.cinevote.ui.theme.CineVoteTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
         setContent {
             CineVoteTheme {
                 // A surface container using the 'background' color from the theme
@@ -30,29 +41,32 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val navController = rememberNavController()
                     NavGraph(navController = navController)
-                    //Greeting("Android")
-                    //LoginForm()
-                    //SignUp()
-                    //HomeScreen()
 
+
+                    val backCallback = remember {
+                        object : OnBackPressedCallback(true) {
+                            override fun handleOnBackPressed() {
+                                if (navController.currentBackStackEntry?.destination?.route == NavigationRoute.HomeScreen.route) {
+                                    finishAffinity()
+                                } else {
+                                    navController.navigateUp()
+                                }
+                            }
+                        }
+                    }
+                    val context = LocalContext.current
+                    DisposableEffect(Unit) {
+                        val dispatcher = (context as ComponentActivity).onBackPressedDispatcher
+                        dispatcher.addCallback(backCallback)
+
+                        onDispose {
+                            backCallback.remove()
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CineVoteTheme {
-        Greeting("Android")
-    }
-}
