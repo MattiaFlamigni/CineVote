ee577c2c15181b002dcd5aba2da766926f7bba39
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-04-26 17:33:12 +0200

    performed icon visualization into topBar due to Screen

--- app/src/main/java/com/example/cinevote/components/TopAppBar.kt
+++ app/src/main/java/com/example/cinevote/components/TopAppBar.kt
@@ -14,24 +14,44 @@ import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.cinevote.NavigationRoute
+import com.example.cinevote.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar(title:String=""){
+fun TopBar(title:String="", navController: NavHostController){
     CenterAlignedTopAppBar(
-        title={Text(title)},
+        title={ Text(
+            text=title,
+            fontFamily = FontFamily.Default,
+            fontSize = 30.sp,
+            letterSpacing = 0.30.sp,
+            color= Color.Black
+
+            ) },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             titleContentColor = Color.Black
         ),
         navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Default.ArrowBack, "back")
+            if(title!= stringResource(id = R.string.home_title)) {
+                IconButton(onClick = { navController.navigateUp() }) {
+                    Icon(Icons.Default.ArrowBack, "back")
+                }
             }
         },
         actions = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Default.Favorite, "Likes")
+
+            if(title!= stringResource(id = R.string.wishlist_title)) {
+                IconButton(onClick = { navController.navigate(NavigationRoute.WishList.route) }) {
+                    Icon(Icons.Default.Favorite, "Likes")
+                }
             }
         }
 
