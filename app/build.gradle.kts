4b70415742cdc75729d59911cf6863b1ceb13069
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-04-29 16:55:22 +0200

    add implementation for chip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.wear.compose:compose-material:1.3.1")
+    implementation("androidx.wear.compose:compose-material-core:1.3.1")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
