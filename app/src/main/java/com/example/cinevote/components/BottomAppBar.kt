cef161e0aebcdff164d39c956abab868206dcacf
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-05-01 00:03:49 +0200

    icon zoom

--- app/src/main/java/com/example/cinevote/components/BottomAppBar.kt
+++ app/src/main/java/com/example/cinevote/components/BottomAppBar.kt
@@ -1,8 +1,11 @@
 package com.example.cinevote.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Home
@@ -10,7 +13,9 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -21,24 +26,23 @@ import com.example.cinevote.NavigationRoute
 @Composable
 fun bottomAppBar(navController: NavHostController) {
     BottomAppBar(
-        containerColor = MaterialTheme.colorScheme.primaryContainer,
-        contentColor = Color.Black,
-        modifier=Modifier.fillMaxWidth(),
+        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+        modifier=Modifier.fillMaxWidth().height(100.dp),
         tonalElevation = 100.dp,
         actions = {
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+                modifier = Modifier.fillMaxWidth().height(70.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
 
             ){
                 simpleChip(Icons.Default.Home, "Home",navController)
-                simpleChip(Icons.Default.Search, "",navController)
+                simpleChip(Icons.Default.Search, "Search",navController)
                 simpleChip(Icons.Default.PlayArrow, "OutNow", navController)
                 simpleChip(Icons.Default.Face, "seen", navController)
 
             }
-
-
         }
     )
 }
