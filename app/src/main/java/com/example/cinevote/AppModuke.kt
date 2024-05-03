df2e153369fa11440b5070756942abe41eaa67eb
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-05-03 12:17:58 +0200

    fix sintax config problems

--- app/src/main/java/com/example/cinevote/AppModuke.kt
+++ app/src/main/java/com/example/cinevote/AppModuke.kt
@@ -2,12 +2,15 @@ package com.example.cinevote
 
 
 import com.example.cinevote.screens.login.LoginViewModel
+import com.example.cinevote.screens.signUp.SignupViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 //val Context.dataStore by preferencesDataStore("theme")
 val appModule = module {
     //single { get<Context>().dataStore }
-    //single { ThemeRepository(get()) }
-    viewModel { LoginViewModel() }
+    single {  }
+    viewModel {  SignupViewModel() }
+
+    viewModel {  LoginViewModel() }
 }
