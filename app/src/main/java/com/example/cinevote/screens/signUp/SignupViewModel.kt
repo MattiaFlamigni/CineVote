fc02df7a49306fe066b1430401d8e69334b94a6e
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-05-03 12:18:27 +0200

    created vm for signup

---
+++ app/src/main/java/com/example/cinevote/screens/signUp/SignupViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.cinevote.screens.signUp
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+data class SignupState(
+    val name:String="",
+    val surname:String="",
+    val username:String="",
+    val mail :String ="",
+    val password:String=""
+)
+
+interface SignUPActions{
+    fun setName(name:String)
+    fun setSurname(surname:String)
+    fun setUsername(username:String)
+    fun setMail(mail:String)
+    fun setPassword(password:String)
+}
+class SignupViewModel : ViewModel() {
+    private val _state = MutableStateFlow(SignupState())
+    val state = _state.asStateFlow()
+
+
+    val action= object :SignUPActions{
+        override fun setName(name: String) {
+            _state.update { it.copy(name=name) }
+        }
+
+        override fun setSurname(surname: String) {
+            _state.update { it.copy(surname=surname) }
+        }
+
+        override fun setUsername(username: String) {
+            _state.update { it.copy(username=username) }
+        }
+
+        override fun setMail(mail: String) {
+            _state.update { it.copy(mail=mail) }
+        }
+
+        override fun setPassword(password: String) {
+            _state.update { it.copy(password=password) }
+        }
+
+    }
+}
