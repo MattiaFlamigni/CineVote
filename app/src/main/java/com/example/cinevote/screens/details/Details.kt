d748b5fa4ba4d82c20f74ad09592db2cc8e49f10
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-05-03 17:00:08 +0200

    reusage space for different information

--- app/src/main/java/com/example/cinevote/screens/details/Details.kt
+++ app/src/main/java/com/example/cinevote/screens/details/Details.kt
@@ -1,12 +1,9 @@
-package com.example.cinevote.screens
+package com.example.cinevote.screens.details
 
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,30 +13,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.CheckCircle
-import androidx.compose.material.icons.outlined.DateRange
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.sharp.Star
 import androidx.compose.material3.AssistChip
-import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -49,7 +36,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -59,12 +45,13 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.cinevote.components.TopBar
 import com.example.cinevote.R
-import com.example.cinevote.components.FilterChipExample
 import com.example.cinevote.components.bottomAppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(navController : NavHostController){
+    var selectedChip by remember { mutableStateOf(ChipOption.TRAMA) }
+
     Scaffold(
         topBar = { TopBar(navController = navController, title= stringResource(R.string.details_title)) },
         bottomBar = { bottomAppBar(navController) },
@@ -161,19 +148,50 @@ fun DetailScreen(navController : NavHostController){
                 horizontalArrangement = Arrangement.Center
             ){
                 item {
-                    AssistChipExample("trama", Icons.Default.Info, {})
-                    AssistChipExample("Cast", Icons.Default.Person, {})
-                    AssistChipExample("Recensioni", Icons.Default.Star, {})
+                    AssistChipExample("trama", Icons.Default.Info, selectedChip==ChipOption.TRAMA){
+                        selectedChip = ChipOption.TRAMA
+                    }
+
+                }
+                item{
+                    AssistChipExample("Cast", Icons.Default.Person, selectedChip==ChipOption.CAST){
+                        selectedChip = ChipOption.CAST
+                    }
+
+                }
+                item{
+                    AssistChipExample("Recensioni", Icons.Default.Star, selectedChip==ChipOption.RECENSIONI){
+                        selectedChip = ChipOption.RECENSIONI
+                    }
                 }
             }
+
+
+            // Contenuto basato sul chip selezionato
+            when(selectedChip) {
+                ChipOption.TRAMA -> {
+                    Text("trama")
+                }
+                ChipOption.CAST -> {
+                    Text("cast")
+                }
+                ChipOption.RECENSIONI -> {
+                    Text("recensioni")
+                }
             }
         }
     }
+}
 
 
 
 @Composable
-private fun AssistChipExample(text:String, icon:ImageVector, onClick:()->Unit) {
+private fun AssistChipExample(
+    text: String,
+    icon: ImageVector,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+) {
     AssistChip(
         modifier = Modifier.padding(10.dp),
         onClick = { onClick() },
@@ -182,9 +200,10 @@ private fun AssistChipExample(text:String, icon:ImageVector, onClick:()->Unit) {
             Icon(
                 icon,
                 contentDescription = "",
-                Modifier.size(AssistChipDefaults.IconSize)
+                Modifier.size(AssistChipDefaults.IconSize),
             )
-        }
+        },
+        /*TODO: ADD COLORS*/
     )
 }
 
