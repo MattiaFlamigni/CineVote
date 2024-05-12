4d1e961f53e790c0625f0072bc308945a6d9391d
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-05-12 23:37:56 +0200

    basic setup for google login

--- app/src/main/java/com/example/cinevote/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/example/cinevote/screens/login/LoginViewModel.kt
@@ -1,13 +1,45 @@
 package com.example.cinevote.screens.login
 
+
+import android.app.Activity
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.cinevote.R
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity.getSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+
+
+
+
+
+import android.content.Intent
+import android.credentials.GetCredentialRequest
+import android.credentials.GetCredentialResponse
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.ui.res.stringResource
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.credentials.CustomCredential
+import androidx.credentials.PasswordCredential
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 
 data class LoginState (
     val mail:String="",
@@ -18,7 +50,7 @@ interface LoginActions{
     fun setMail(mail:String)
     fun setPassword(password:String)
     fun isKeyCorrect(mail: String, password: String, onCompleteListener: (Boolean) -> Unit)
-
+    fun loginGoogle()
 }
 class LoginViewModel: ViewModel() {
     private val _state= MutableStateFlow(LoginState())
@@ -26,14 +58,18 @@ class LoginViewModel: ViewModel() {
 
     val actions = object : LoginActions {
         override fun setMail(mail: String) {
-            _state.update { it.copy(mail=mail) }
+            _state.update { it.copy(mail = mail) }
         }
 
         override fun setPassword(password: String) {
-            _state.update { it.copy(password=password) }
+            _state.update { it.copy(password = password) }
         }
 
-        override fun isKeyCorrect(mail: String, password: String, onCompleteListener: (Boolean) -> Unit) {
+        override fun isKeyCorrect(
+            mail: String,
+            password: String,
+            onCompleteListener: (Boolean) -> Unit
+        ) {
             val auth: FirebaseAuth = Firebase.auth
 
             auth.signInWithEmailAndPassword(mail, password)
@@ -51,6 +87,30 @@ class LoginViewModel: ViewModel() {
         }
 
 
+        override fun loginGoogle() {
+        }
     }
-
 }
+
+
+
+
+private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+    try {
+        val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+        // L'accesso è stato eseguito correttamente, puoi utilizzare 'account' per ottenere informazioni sull'utente
+
+        // Esempio di utilizzo dei dati dell'account:
+        val idToken = account?.idToken // Ottieni l'idToken per l'autenticazione del backend
+        val email = account?.email // Ottieni l'email dell'utente
+
+        // Ora puoi utilizzare l'idToken per l'autenticazione lato server (se necessario)
+
+    } catch (e: ApiException) {
+        // L'accesso non è riuscito, gestisci l'errore appropriatamente
+        Log.w("TAG", "signInResult:failed code=" + e.statusCode)
+    }
+}
+
+
+
