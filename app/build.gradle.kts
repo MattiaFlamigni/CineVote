cddc69ff8ebe296127ca9f9d241d4112e430bf46
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-05-12 21:48:43 +0200

    updated UI for google login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
--- app/src/main/java/com/example/cinevote/screens/login/Login.kt
+++ app/src/main/java/com/example/cinevote/screens/login/Login.kt
@@ -1,7 +1,11 @@
 package com.example.cinevote.screens.login
 
 import android.util.Log
+import android.view.RoundedCorner
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,8 +15,16 @@ import androidx.compose.foundation.layout.fillMaxHeight
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,7 +33,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -122,10 +137,40 @@ fun LoginScreen(
                     modifier=Modifier.weight(1f), fontSize = 20.sp)
             }
 
+            LoginWithSocial()
+
+
+
         }
 
     }
 }
+
+
+
+
+@Composable
+private fun LoginWithSocial(){
+
+    Row(
+        modifier= Modifier.fillMaxWidth().padding(20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.google),
+            contentDescription = "Google",
+            modifier = Modifier
+                .size(60.dp).clip(CircleShape )
+                .clickable {
+                    /*TODO*/
+                }
+        )
+
+    }
+}
+
+
 
 
 
