89bea4078667c0fd24d75f52ddfa2ef58e270118
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-07-17 17:19:14 +0200

    code intendation

--- app/src/main/java/com/example/cinevote/badge/BadgeVM.kt
+++ app/src/main/java/com/example/cinevote/badge/BadgeVM.kt
@@ -1,21 +1,15 @@
 package com.example.cinevote.badge
 
-import android.media.Image
-import android.net.Uri
 import android.util.Log
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import com.example.cinevote.screens.settings.SettingsStatus
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import com.example.cinevote.R
 import com.example.cinevote.data.Review
 import com.example.cinevote.data.database.Firestore
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 data class Badge(
     val name: String,
@@ -27,16 +21,13 @@ data class Badge(
 )
 
 enum class BadgeType {
-    WATCH_MOVIES,
-    WRITE_REVIEWS,
-    EARN_POINTS,
+    WATCH_MOVIES, WRITE_REVIEWS, EARN_POINTS,
     // Aggiungi altri tipi di badge qui
 }
 
 
 data class BadgeStatus(
-    val badgeList: List<Badge> = emptyList(),
-    val achievedBadges: List<Badge> = emptyList()
+    val badgeList: List<Badge> = emptyList(), val achievedBadges: List<Badge> = emptyList()
 )
 
 interface BadgeAction {
@@ -58,10 +49,10 @@ class BadgeViewModel : ViewModel() {
 
 
         override fun checkBadgeReached(badge: Badge) {
-            viewModelScope.launch{
-            var username = firestore.actions.getDataFromMail("username")
+            viewModelScope.launch {
+                var username = firestore.actions.getDataFromMail("username")
 
-                if(username.isEmpty()){
+                if (username.isEmpty()) {
                     username = auth.currentUser?.displayName.toString()
                 }
 
@@ -74,15 +65,15 @@ class BadgeViewModel : ViewModel() {
                     }
 
                     BadgeType.WRITE_REVIEWS -> {
-                        val list : MutableList<Review> = mutableListOf()
+                        val list: MutableList<Review> = mutableListOf()
                         val reviews = firestore.actions.getReviewByUser(username)
-                        for(review in reviews){
-                            if(review.descrizione.isNotEmpty()){
+                        for (review in reviews) {
+                            if (review.descrizione.isNotEmpty()) {
                                 list.add(review)
                             }
                         }
 
-                        list.size>=5
+                        list.size >= 5
                     }
 
                     BadgeType.EARN_POINTS -> {
@@ -115,8 +106,7 @@ class BadgeViewModel : ViewModel() {
                     type = BadgeType.WATCH_MOVIES,
                     condition = "Guarda 10 film",
                     effect = "Ottieni un badge Film Lover"
-                ),
-                Badge(
+                ), Badge(
                     name = "Critico",
                     description = "Scrivi 5 recensioni",
                     image = R.drawable.star,
